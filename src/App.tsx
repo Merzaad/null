ada01beb34c760bbff3bdd9c04659f5f07836360
merzaad <merzaad@gmail.com>
2023-03-08 07:17:12 +0330

    initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,17 @@ import LoadingOrError from 'components/LoadingOrError'
 import type { ReactElement } from 'react'
 import { lazy, Suspense } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
+/* 
 const Gallery = lazy(async () => import('pages/Gallery'))
-const Details = lazy(async () => import('pages/Details'))
+const Details = lazy(async () => import('pages/Details')) */
+const Test = lazy(async () => import('pages/Test'))
 
 export default function App(): ReactElement {
 	return (
 		<BrowserRouter>
 			<Suspense fallback={<LoadingOrError />}>
 				<Routes>
-					<Route path='/' element={<Gallery />} />
-					<Route path=':fruitName' element={<Details />} />
+					<Route path='/' element={<Test />} />
 				</Routes>
 			</Suspense>
 		</BrowserRouter>
